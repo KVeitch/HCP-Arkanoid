1f12499b27204c7357ed3a3658da803f0bbceb06
yaairfernando <yair.facio11@gmail.com>
2023-02-10 16:01:52 -0600

    Add paddle styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Paddle from './components/Paddle/Paddle'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/components/Paddle/Paddle.tsx
+++ src/components/Paddle/Paddle.tsx
@@ -1,7 +1,21 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core";
 import PADDLE from "../../assets/images/paddle.png";
 
+const useStyles = makeStyles({
+  paddle: {
+    width: "150px",
+    height: "20px",
+    position: "absolute",
+    left: '50%',
+    top: '90%',
+  },
+});
+
 const Paddle = () => {
-  return <img src={PADDLE} />;
+  const { paddle } = useStyles();
+
+  return <img src={PADDLE} className={paddle} id="paddle" />;
 };
 
 export default Paddle;
