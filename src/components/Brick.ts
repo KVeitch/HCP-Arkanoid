66daf84449bf96206efc9456d338a0f6ed9138f0
Kirk Veitch <kirk.veitch@housecallpro.com>
2023-02-23 14:14:17 -0700

    Correct naming

--- src/components/Brick.ts
+++ src/components/Brick.ts
@@ -1,43 +1,43 @@
 import { Vector } from './types'
 
 export class Brick {
+  private brickImage: HTMLImageElement = new Image();
+
   constructor(
-    brickWidth: number,
-    brickHeight: number,
-    position: Vector,
-    brickEnergy: number,
-    image: string,
+    private brickWidth: number,
+    private brickHeight: number,
+    private position: Vector,
+    private brickEnergy: number,
+    image: string
   ) {
-    this.brickHeight = brickHeight;
     this.brickWidth = brickWidth;
-    this.brickEnergy = brickEnergy;
+    this.brickHeight = brickHeight;
     this.position = position;
+    this.brickEnergy = brickEnergy;
     this.brickImage.src = image;
   }
+
   get width(): number {
-    return this.brickWidth
-  }
-  get height(): number {
-    return this.brickHeight
-  }
-  get image(): HTML_ImageElement {
-    return this.brickImage
+    return this.brickWidth;
   }
 
-  get position(): Vector {
-    return this.position
+  get height(): number {
+    return this.brickHeight;
+  }
+
+  get pos(): Vector {
+    return this.position;
+  }
+
+  get image(): HTMLImageElement {
+    return this.brickImage;
   }
 
   get energy(): number {
-    return this.brickEnergy
+    return this.brickEnergy;
   }
 
-  set energy(energy:number) {
-    this.brickEnergy = energy
+  set energy(energy: number) {
+    this.brickEnergy = energy;
   }
-
-  reduceEnergy() {
-    this.brickEnergy -= 1
-  }
-
 }
