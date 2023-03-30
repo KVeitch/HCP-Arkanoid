bfe6e33d0b2492ce0ad84eb79afa4a96152666dc
Kirk Veitch <kirk.veitch@housecallpro.com>
2023-03-30 13:36:52 -0600

    Fix board and change ball

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 import PADDLE_IMAGE from './assets/images/paddle.png';
-import BALL_IMAGE from './assets/images/ball.png';
+import BALL_IMAGE from './assets/images/react.svg';
+// import BALL_IMAGE from './assets/images/ball.png';
 
-// import Paddle from './components/Paddle/Paddle'
 import { Ball } from './components/Ball';
 import { Brick } from './components/Brick';
 import { CanvasView } from './components/CanvasView';
